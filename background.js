0744184b28efabb22da0debb62f0e2b6fac44ff7
Xinyi Chen <xinyi@uber.com>
2018-03-24 03:15:42 -0700

    Initial commit

---
+++ background.js
@@ -0,0 +1,7 @@
+chrome.tabs.onUpdated.addListener(
+  function (tabId, changeInfo, tab) {
+    if (tab.url.startsWith('https://calendar.google.com/calendar/r/eventedit') && changeInfo.status === 'complete') {
+      chrome.tabs.sendMessage(tabId, {text: 'event_edit'});
+    }
+  }
+);
