b0954f3a3fa0ee57ff29f9da812493b2e10a7d2e
Xinyi Chen <xinyi@uber.com>
2018-09-09 16:26:49 -0700

    break down logic

--- script/background/auto-room-searcher.js
+++ script/background/auto-room-searcher.js
@@ -5,38 +5,55 @@ chrome.tabs.create({
   pinned: true
 }, tab => workerTabId = tab.id);
 
-/* ===================== Auto Room Searching Logic ====================== */
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg.type === ROOM_SELECTED) {
-    // todo: filter by event id
-    searchRooms();
-  }
-});
+// todo: add start and stop control on settings
+// todo: deal with deleted events
+// https://calendar.google.com/calendar/r?msg=The+requested+event+has+been+cancelled+and+removed+from+your+calendar.&msgtok=24f0c00ea165bd51063a9d36a2d76ee2fed84987
+// todo: deal with non-editable events
+// https://calendar.google.com/calendar/r/eventedit/Nm5uNWdzNGJkMTlrOTczcjg2dXBobDRsb3MgdWJlci5jb21fM3JycGtoZGRuazY2MTRycW5kbTVzMnFoMHNAZw
+// todo: deal with 403 error
+// https://calendar.google.com/calendar/r/eventedit/Nmw1cHA2YnAxNmhtbGl0cWx1OGthdmNjNWtfMjAxODEwMTFUMjIwMDAwWiB4aW55aUB1YmVyLmNvbQ
+// todo: think about network disruption
+// todo: resurrect dead cycles in an event based implementation
 
-function searchRooms() {
+function hibernate() {
+  return setTimeout(nextItem, 5 * 60 * 1000);
+}
+
+function nextItem() {
   if (toBeFulfilled.length === 0) {
-    // if there's no backlog, check again in 5 min
-    return setTimeout(searchRooms, 5 * 60 * 1000);
+    hibernate();
+  } else {
+    loadEvent(toBeFulfilled.shift());
   }
+}
 
-  // todo: deal with deleted events
-  // https://calendar.google.com/calendar/r?msg=The+requested+event+has+been+cancelled+and+removed+from+your+calendar.&msgtok=24f0c00ea165bd51063a9d36a2d76ee2fed84987
-  // todo: deal with non-editable events
-  // https://calendar.google.com/calendar/r/eventedit/Nm5uNWdzNGJkMTlrOTczcjg2dXBobDRsb3MgdWJlci5jb21fM3JycGtoZGRuazY2MTRycW5kbTVzMnFoMHNAZw
-  // todo: deal with 403 error
-  // https://calendar.google.com/calendar/r/eventedit/Nmw1cHA2YnAxNmhtbGl0cWx1OGthdmNjNWtfMjAxODEwMTFUMjIwMDAwWiB4aW55aUB1YmVyLmNvbQ
-  // todo: think about network disruption
-  // todo: resurrect dead cycles in an event based implementation
+function loadEvent(eventId) {
+  const urlToLoad = `${EDIT_PAGE_URL_PREFIX}/${toBeFulfilled.shift()}`;
+
+  function pageLoadListener(tabId, changeInfo, tab) {
+    const isWorker = tabId === workerTabId;
+    const isTargetUrl = tab.url === urlToLoad;
+    const isLoaded = changeInfo.status === "complete";
+
+    if (isWorker && isTargetUrl && isLoaded) {
+      emit(workerTabId, {type: AUTO_ROOM_BOOKING});
+      chrome.tabs.onUpdated.removeListener(pageLoadListener);
+    }
+  }
+  chrome.tabs.onUpdated.addListener(pageLoadListener);
+
   chrome.tabs.update(workerTabId, {
-    url: `${EDIT_PAGE_URL_PREFIX}/${toBeFulfilled.shift()}`,
+    url: urlToLoad,
     active: false,
     pinned: true
   });
-
-  // todo: change to event based
-  setTimeout(() => emit(workerTabId, {type: AUTO_ROOM_BOOKING}), 2000);
 }
 
-searchRooms();
+onMessage((msg, sender, sendResponse) => {
+  if (msg.type === ROOM_SELECTED) {
+    // todo: filter by event id
+    nextItem();
+  }
+});
 
-// todo: add start and stop control on settings
+nextItem();
