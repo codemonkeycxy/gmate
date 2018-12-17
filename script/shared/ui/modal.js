9a8426ad9c1c41ec4e42816e845176e23c6b3cf1
Xinyi Chen <xinyi@uber.com>
2018-12-17 16:46:48 +0800

    render modal UI first draft

---
+++ script/shared/ui/modal.js
@@ -0,0 +1,44 @@
+/**
+ * it's a shame that there's no good way to dynamically create elements from html file in chrome extension
+ * (see a hacky way in here: https://stackoverflow.com/questions/19103183/how-to-insert-html-with-a-chrome-extension)
+ *
+ * the best way i can see now is to manually assembles the necessary component via code.
+ * the following function returns something like the following html
+ * (html and styling reference: https://www.w3schools.com/howto/howto_css_modals.asp)
+
+ <div class="modal">
+     <div class="modal-content">
+         <span class="modal-close">&times;</span>
+         <p>Some text in the Modal..</p>
+     </div>
+ </div>
+
+ */
+function renderModal() {
+  const modal = document.createElement('div');
+  modal.className = 'modal';
+
+  const modalContent = document.createElement('div');
+  modalContent.className = 'modal-content';
+
+  const closeBtn = document.createElement('span');
+  closeBtn.className = 'modal-close';
+  closeBtn.textContent = '&times;';
+  closeBtn.onclick = () => modal.style.display = "none";
+
+  const modalText = document.createElement('p');
+  modalText.textContent = 'Some text in the Modal..';
+
+  modalContent.appendChild(closeBtn);
+  modalContent.appendChild(modalText);
+  modal.appendChild(modalContent);
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = event => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  return modal;
+}
