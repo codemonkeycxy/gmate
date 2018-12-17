bf2c4e0abab49bb1e665d4d5f910a1477c9f44c7
Xinyi Chen <xinyi@uber.com>
2018-12-17 15:06:07 +0800

    move in-code styling to main.css

--- script/content/add-overlay.js
+++ script/content/add-overlay.js
@@ -7,18 +7,9 @@
       return;
     }
 
+    // see styling folder for overlay styling
     const overlay = document.createElement("div");
     overlay.id = overlayId;
-    overlay.style.position = 'fixed';
-    overlay.style.width = '100%';
-    overlay.style.height = '100%';
-    overlay.style.top = '0';
-    overlay.style.left = '0';
-    overlay.style.right = '0';
-    overlay.style.bottom = '0';
-    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-    overlay.style.zIndex = '99999';
-    overlay.style.cursor = 'not-allowed';
 
     insertBefore(overlay, document.body.firstChild);
   }
