499cac6ac6ef2c7e4f86876b57e761441e1a519e
Xinyi Chen <xinyi@uber.com>
2018-12-19 14:32:45 +0800

    generate modal footer (1st draft)

--- script/shared/ui/modal.js
+++ script/shared/ui/modal.js
@@ -14,11 +14,15 @@
          <div class="modal-body">
              <!--modal body to be injected according to the passed in parameter-->
          </div>
+         <div class="modal-footer">
+             <button>Cancel</button>
+             <button>OK</button>
+         </div>
      </div>
  </div>
 
  */
-function renderModal(body, headerText) {
+function renderModal(body, headerText, onConfirm, onCancel) {
   const modal = document.createElement('div');
   modal.className = 'modal';
 
@@ -28,21 +32,8 @@ function renderModal(body, headerText) {
 
   _injectModalHeader(modalContent, headerText);
   _injectModalBody(modalContent, body);
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = event => {
-    if (event.target === modal) {
-      modal.style.display = "none";
-    }
-  };
-
-  // When the user presses escape, close the modal
-  document.onkeydown = evt => {
-    evt = evt || window.event;
-    if (evt.keyCode === 27) {
-      modal.style.display = "none";
-    }
-  };
+  _injectModalFooter(modal, modalContent, onConfirm, onCancel);
+  _handleCloseModalUX(modal);
 
   return modal;
 }
@@ -68,4 +59,43 @@ function _injectModalBody(modalContent, body) {
   modalBody.appendChild(body);
 
   modalContent.appendChild(modalBody);
+}
+
+function _injectModalFooter(modal, modalContent, onConfirm, onCancel) {
+  const footerWrapper = document.createElement('div');
+  footerWrapper.className = 'modal-footer';
+  modalContent.appendChild(footerWrapper);
+
+  const cancelBtn = document.createElement('button');
+  cancelBtn.textContent = 'Cancel';
+  cancelBtn.onclick = () => {
+    onCancel && onCancel();
+    modal.style.display = "none";
+  };
+  footerWrapper.appendChild(cancelBtn);
+
+  const confirmBtn = document.createElement('button');
+  confirmBtn.textContent = 'OK';
+  cancelBtn.onclick = () => {
+    onConfirm && onConfirm();
+    modal.style.display = "none";
+  };
+  footerWrapper.appendChild(confirmBtn);
+}
+
+function _handleCloseModalUX(modal) {
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = event => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  // When the user presses escape, close the modal
+  document.onkeydown = evt => {
+    evt = evt || window.event;
+    if (evt.keyCode === 27) {
+      modal.style.display = "none";
+    }
+  };
 }
