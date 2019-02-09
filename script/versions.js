743f02dee34d89cd63fe292ce7b24ec865f1b309
Xinyi Chen <xinyi@uber.com>
2019-02-09 11:20:08 -0800

    release v0.4.1

--- script/versions.js
+++ script/versions.js
@@ -1,4 +1,5 @@
 const VERSIONS = {
+  '0.4.1': {notify: {url: 'https://www.gmate.us/announcement/v0-4-1'}},
   '0.4.0': {notify: {url: 'https://www.gmate.us/announcement/v0-4-0'}},
   '0.3.3': {notify: {url: 'https://www.gmate.us/announcement/v0-3-3'}},
   '0.3.2': {notify: {url: 'https://www.gmate.us/announcement/v0-3-2'}},
