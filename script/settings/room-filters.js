9a832212b6ad8655e5bc08d550a413e5c34f6c24
Xinyi Chen <xinyi@uber.com>
2019-06-08 11:26:35 -0700

    v3.0.3

--- script/settings/room-filters.js
+++ script/settings/room-filters.js
@@ -33,7 +33,7 @@ const COMPANY_SPECIFIC_FILTERS = {
 
       return {
         valid: false,
-        errMsg: "Starting from June 24, 2019, location selection will be required. If your office address is not listed in the dropdown, email xinyichencxy@gmail.com"
+        errMsg: "Starting from June 17, 2019, location selection will be required. If your office address is not listed in the dropdown, email xinyichencxy@gmail.com"
       }
     }
   }, {
