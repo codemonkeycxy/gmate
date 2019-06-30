125152af5de0fda2a61779e04ec7b5848c2fa41c
Xinyi Chen <xinyi@uber.com>
2019-06-29 19:36:36 -0700

    refactor into helper

--- script/background/room-radar.js
+++ script/background/room-radar.js
@@ -8,37 +8,45 @@
   // todo: add a loader while the async call happens https://www.w3schools.com/howto/howto_css_loader.asp
   // todo: inject icon
 
-  const eventFilters = {
-    flexFilters: {
-      'room-booking-filter-negative': "",
-      'room-booking-filter-positive-1': "",
-      'room-booking-filter-uber-floor': "4,5",
-      'room-booking-filter-uber-location': "SFO | 1455 Market",
-      'room-booking-filter-uber-need_vc': true,
-      'room-booking-filter-uber-room_size': "3-15",
-    },
-    negFilter: "",
-    posFilter: "",
-  };
-  const {posFilter, negFilter, flexFilters} = eventFilters;
+  // todo: put a sane limit (maybe 5)
+  const container = dom.getElementById('underutilized-rooms');
+  container.appendChild(await buildResultForEvent());
 
-  const event = await CalendarAPI.getEventB64('N2owYXM2ZzdxN3N1aDlzdTVpaGM5YmhwYXYgeGlueWlAdWJlci5jb20');
-  const allRooms = await getFullRoomList();
-  const roomCandidates = allRooms.filter(room => matchRoom(room.name, posFilter, negFilter, flexFilters));
+  async function buildResultForEvent() {
+    const eventFilters = {
+      flexFilters: {
+        'room-booking-filter-negative': "",
+        'room-booking-filter-positive-1': "",
+        'room-booking-filter-uber-floor': "4,5",
+        'room-booking-filter-uber-location': "SFO | 1455 Market",
+        'room-booking-filter-uber-need_vc': true,
+        'room-booking-filter-uber-room_size': "3-15",
+      },
+      negFilter: "",
+      posFilter: "",
+    };
+    const {posFilter, negFilter, flexFilters} = eventFilters;
 
-  const busyRooms = await CalendarAPI.pickBusyRooms(event.startStr, event.endStr, roomCandidates.map(room => room.email));
-  console.log(busyRooms);
-  // todo: add sane limit for busy rooms
-  const events = await CalendarAPI.getEventsForRooms(event.startStr, event.endStr, busyRooms);
-  console.log(events);
-  const candidates = events.filter(event => event.name && event.name.includes('1:1'));
+    const event = await CalendarAPI.getEventB64('N2owYXM2ZzdxN3N1aDlzdTVpaGM5YmhwYXYgeGlueWlAdWJlci5jb20');
+    const allRooms = await getFullRoomList();
+    const roomCandidates = allRooms.filter(room => matchRoom(room.name, posFilter, negFilter, flexFilters));
 
-  const list = newList(candidates.map(event =>
-    `<li>
+    const busyRooms = await CalendarAPI.pickBusyRooms(event.startStr, event.endStr, roomCandidates.map(room => room.email));
+    console.log(busyRooms);
+    // todo: add sane limit for busy rooms
+    const events = await CalendarAPI.getEventsForRooms(event.startStr, event.endStr, busyRooms);
+    console.log(events);
+    const candidates = events.filter(event => event.name && event.name.includes('1:1'));
+
+    const myEvent = htmlToElement(`<p>${event.name}</p>`);
+    const theirEvents = newList(candidates.map(event =>
+      `<li>
         <a href=${event.htmlLink} target="_blank">
-            ${event.name || "unnamed event"}
+            ${event.name || 'unnamed event'}
         </a>
     </li>`
-  ));
-  dom.getElementById('to-inject').appendChild(list);
+    ));
+
+    return wrapUIComponents([myEvent, theirEvents]);
+  }
 })();
