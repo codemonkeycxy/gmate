6598041085b479535549b8703b65357245fd1600
Xinyi Chen <xinyi@uber.com>
2019-08-31 09:22:59 -0700

    simplify room filter UI rendering by html injection

--- script/shared/ui/room-booking-filters.js
+++ script/shared/ui/room-booking-filters.js
@@ -1,28 +1,3 @@
-/**
- * it's a shame that there's no good way to dynamically create elements from html file in chrome extension
- * (see a hacky way in here: https://stackoverflow.com/questions/19103183/how-to-insert-html-with-a-chrome-extension)
- *
- * the best way i can see now is to manually assembles the necessary component via code.
- * this function returns the following html
-
- <div>
- <!--new style flex filters to be dynamically generated via code-->
-
- <!--old style regex filters-->
- <span>Name include (</span>
- <a href="https://regex101.com/" target="_blank">regex</a>
- <span>): </span>
- <input type="text" placeholder="1455.*0(4|5)th" room-booking-filter-positive-1">
-
- <br>
-
- <span>Name exclude (</span>
- <a href="https://regex101.com/" target="_blank">regex</a>
- <span>): </span>
- <input type="text" placeholder="(Cart|Quiet Room)" id="room-booking-filter-negative">
- </div>
-
- */
 async function asyncRenderRoomBookingFilters(onChange) {
   const FILTER_RENDER_FUNCTIONS = {
     [AUTO_COMPLETE]: renderAutoCompleteFilter,
@@ -31,77 +6,17 @@ async function asyncRenderRoomBookingFilters(onChange) {
     [CHECKBOX]: renderCheckboxFilter,
   };
 
-  /**
-   * generates the following html
+  async function injectRegexFiltersUI(filtersWrapper, posFilter, negFilter) {
+    const regexFilters = await loadHTMLElement('template/room-filters.html');
+    filtersWrapper.appendChild(regexFilters);
 
-   <span>Name include (</span>
-   <a href="https://regex101.com/" target="_blank">regex</a>
-   <span>): </span>
-   <input type="text" placeholder="1455.*0(4|5)th" room-booking-filter-positive-1">
-
-   <br>
-
-   <span>Name exclude (</span>
-   <a href="https://regex101.com/" target="_blank">regex</a>
-   <span>): </span>
-   <input type="text" placeholder="(Cart|Quiet Room)" id="room-booking-filter-negative">
-
-   */
-  function injectRegexFiltersUI(filtersWrapper, posFilter, negFilter) {
-    // <span>Name include (</span>
-    const textSpan1 = document.createElement('span');
-    textSpan1.innerText = 'Name include (';
-    filtersWrapper.appendChild(textSpan1);
-
-    // <a href="https://regex101.com/" target="_blank">regex</a>
-    const regexLink1 = document.createElement('a');
-    regexLink1.href = 'https://regex101.com/';
-    regexLink1.target = '_blank';
-    regexLink1.textContent = 'regex';
-    filtersWrapper.appendChild(regexLink1);
-
-    // <span>): </span>
-    const textSpan2 = document.createElement('span');
-    textSpan2.innerText = '): ';
-    filtersWrapper.appendChild(textSpan2);
-
-    // <input type="text" placeholder="1455.*0(4|5)th" id="room-booking-filter-positive-1">
-    const posFilterInput = document.createElement('input');
-    posFilterInput.type = 'text';
-    posFilterInput.placeholder = '1455.*0(4|5)th';
-    posFilterInput.id = ROOM_BOOKING_FILTER_POSITIVE;
+    const posFilterInput = findChildById(regexFilters, ROOM_BOOKING_FILTER_POSITIVE);
     posFilterInput.value = posFilter;
     posFilterInput.addEventListener("input", e => onChange(ROOM_BOOKING_FILTER_POSITIVE, e.target.value));
-    filtersWrapper.appendChild(posFilterInput);
 
-    // <br>
-    filtersWrapper.appendChild(document.createElement('br'));
-
-    // <span>Name exclude (</span>
-    const textSpan3 = document.createElement('span');
-    textSpan3.innerText = 'Name exclude (';
-    filtersWrapper.appendChild(textSpan3);
-
-    // <a href="https://regex101.com/" target="_blank">regex</a>
-    const regexLink2 = document.createElement('a');
-    regexLink2.href = 'https://regex101.com/';
-    regexLink2.target = '_blank';
-    regexLink2.textContent = 'regex';
-    filtersWrapper.appendChild(regexLink2);
-
-    // <span>): </span>
-    const textSpan4 = document.createElement('span');
-    textSpan4.innerText = '): ';
-    filtersWrapper.appendChild(textSpan4);
-
-    //  <input type="text" placeholder="(Cart|Quiet Room)" id="room-booking-filter-negative">
-    const negFilterInput = document.createElement('input');
-    negFilterInput.type = 'text';
-    negFilterInput.placeholder = '(Cart|Quiet Room)';
-    negFilterInput.id = ROOM_BOOKING_FILTER_NEGATIVE;
+    const negFilterInput = findChildById(regexFilters, ROOM_BOOKING_FILTER_NEGATIVE);
     negFilterInput.value = negFilter;
     negFilterInput.addEventListener("input", e => onChange(ROOM_BOOKING_FILTER_NEGATIVE, e.target.value));
-    filtersWrapper.appendChild(negFilterInput);
   }
 
   function injectFlexFiltersUI(filtersWrapper, flexFilters) {
@@ -167,7 +82,7 @@ async function asyncRenderRoomBookingFilters(onChange) {
 
   const {posFilter, negFilter, flexFilters} = await getRoomFilters();
   injectFlexFiltersUI(filtersWrapper, flexFilters);
-  injectRegexFiltersUI(filtersWrapper, posFilter, negFilter);
+  await injectRegexFiltersUI(filtersWrapper, posFilter, negFilter);
 
   return filtersWrapper;
 }
