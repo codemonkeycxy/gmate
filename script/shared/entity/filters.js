2405d11498b789c609e842ee693642fe4156af55
Xinyi Chen <xinyi@uber.com>
2019-08-31 08:32:00 -0700

    move filter fetching logic into filters entity file

--- script/shared/entity/filters.js
+++ script/shared/entity/filters.js
@@ -5,3 +5,34 @@ class Filters {
     this.flexFilter = flexFilters;
   }
 }
+
+function getRoomFilterStorageKey(filterKey) {
+  return `room-booking-filter-${'uber'}-${filterKey}`;
+}
+
+/**
+ * Flex filters are room filters that are flexibly defined on a per company basis.
+ * See examples from the COMPANY_SPECIFIC_FILTERS on the above
+ */
+async function getFlexRoomFilters() {
+  const companyName = 'uber';  // hard code for now
+  const filterSettings = COMPANY_SPECIFIC_FILTERS[companyName];
+  const storageKeys = {};
+  filterSettings.forEach(setting => storageKeys[getRoomFilterStorageKey(setting.key)] = setting.default);
+
+  return await getFromSync(storageKeys);
+}
+
+/**
+ * Returns both the old style regex based filters and the new style company specific flex filters
+ * @returns {Promise<{posFilter: *, negFilter: *, flexFilters: *}>}
+ */
+async function getRoomFilters() {
+  const result = await getFromSync(DEFAULT_ROOM_BOOKING_FILTERS);
+
+  return {
+    posFilter: result[ROOM_BOOKING_FILTER_POSITIVE],
+    negFilter: result[ROOM_BOOKING_FILTER_NEGATIVE],
+    flexFilters: await getFlexRoomFilters()
+  }
+}
