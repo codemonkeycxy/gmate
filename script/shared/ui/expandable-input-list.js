ff658970b8d89d24645f1129650c2727a7d70563
Xinyi Chen <xinyi@uber.com>
2019-09-06 22:59:53 -0700

    improve expandable input list UI

--- script/shared/ui/expandable-input-list.js
+++ script/shared/ui/expandable-input-list.js
@@ -1,29 +1,31 @@
 // ref: https://stackoverflow.com/questions/42476463/add-remove-input-box-using-javascript
 function newExpandableInputList(values, onChange) {
-  const wrapper = document.createElement('div');
-  const addBtn = newButton('+');
-  const removeBtn = newButton('-');
+  const listWrapper = document.createElement('div');
 
-  wrapper.appendChild(addBtn);
-  wrapper.appendChild(removeBtn);
+  function addRow(id) {
+    const rowWrapper = document.createElement('div');
+    rowWrapper.id = `expandable-input-list-item-${id}`;
+    listWrapper.appendChild(rowWrapper);
 
-  addBtn.addEventListener("click", add);
-  removeBtn.addEventListener("click", remove);
-
-  function add() {
     const input = document.createElement("INPUT");
     input.setAttribute("type", "text");
     input.setAttribute("placeholder", "Name");
-    input.setAttribute("class", "expandable-input-list-item");
-    wrapper.appendChild(input);
+    rowWrapper.appendChild(input);
+
+    const addBtn = newButton('+');
+    addBtn.addEventListener("click", () => addRow(id + 1));
+    rowWrapper.appendChild(addBtn);
+
+    const removeBtn = newButton('-');
+    removeBtn.addEventListener("click", () => removeRow(id));
+    rowWrapper.appendChild(removeBtn);
   }
 
-  function remove() {
-    const children = wrapper.getElementsByClassName("expandable-input-list-item");
-    if(children.length > 0) {
-      wrapper.removeChild(children[children.length - 1]);
-    }
+  function removeRow(id) {
+    const toRemove = findChildById(listWrapper, `expandable-input-list-item-${id}`);
+    toRemove.remove();
   }
 
-  return wrapper;
+  addRow(0);
+  return listWrapper;
 }
