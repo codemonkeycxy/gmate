fa0908241124a5e838dd9d13d0e6d69fbeec0399
Xinyi Chen <xinyi@uber.com>
2019-10-12 17:02:18 -0700

    task display header show/hide

--- script/content/add-gmate-btn/task-display.js
+++ script/content/add-gmate-btn/task-display.js
@@ -1,9 +1,11 @@
 function newTaskDisplay() {
   const header = htmlToElement('<div>GMate is currently searching room for the following events:</div>');
   const body = document.createElement('div');
+  hide(header);
 
   const taskDisplay = wrapUIComponents([header, body]);
   taskDisplay.pushTask = eventName => {
+    show(header);
     body.appendChild(htmlToElement(`<div>${eventName}</div>`));
   };
 
