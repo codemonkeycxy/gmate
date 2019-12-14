9ec704db69b57c966c3bd8b6d5270ad2dc0a82f3
Xinyi Chen <xinyi@uber.com>
2019-12-14 10:37:29 -0800

    add gmate row when dialog is added

--- script/content/add-gmate-btn/on-main-page.js
+++ script/content/add-gmate-btn/on-main-page.js
@@ -1,5 +1,22 @@
 // self-invoking function to avoid name collision
 (() => {
+  function isDialogAdded(mutationRecords) {
+    return mutationRecords.some(record => {
+      if (record.addedNodes.length === 0) {
+        return false;
+      }
+
+      for (let i = 0; i < record.addedNodes.length; i++) {
+        const addedNode = record.addedNodes[i];
+        if (addedNode.className === 'aZpV8b iWO5td') {
+          return true;
+        }
+      }
+
+      return false;
+    });
+  }
+
   function getDialog() {
     return getElementByAttr('div', 'role', 'dialog');
   }
@@ -18,8 +35,8 @@
     }
   }
 
-  function insertSearchRoomBtn(dialog) {
-    const gmateRow = newGMateBtn('123');
+  async function insertSearchRoomBtn(dialog) {
+    const gmateRow = await newGMateRow('123', noop());
 
     // if (isMeetingCreationDialog(dialog)) {
     //   const eventOptionRows = getCreateEventOptions(dialog);
@@ -38,23 +55,29 @@
     }
   }
 
-  function handleMeetingCreationClick() {
+  async function handleMeetingCreationClick(mutationRecords) {
+    if (!isDialogAdded(mutationRecords)) {
+      return;
+    }
+
     const dialog = getDialog();
     if (!dialog) {
       return;
     }
 
+    console.log(mutationRecords);
+
     if (getChildById(dialog, GMATE_BTN_ID)) {
       return;
     }
 
-    insertSearchRoomBtn(dialog);
+    await insertSearchRoomBtn(dialog);
   }
 
   if (/calendar.google.com/g.test(window.location.host)) {
     const observer = new MutationObserver(handleMeetingCreationClick);
     observer.observe(document, {
-      attributes: true,
+      attributes: false,
       childList: true,
       subtree: true
     });
